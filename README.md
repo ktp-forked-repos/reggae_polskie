ba02a57c5aad3705aa95225b6dc518ac9bd90b29
MaciejPowallo <maciej.powallo@10g.pl>
2017-10-15 01:48:58 +0200

    version 4.0.3

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 name: Reggae Polskie,
 author: Maciej Powallo,
-"CMS": 1.2.1,
-version: 4.0.2,
+"CMS": 1.2.2,
+version: 4.0.3,
 license: MIT,
 created at: 10.2017
 updated at: 10.2017
--- controllers/bands_item.php
+++ controllers/bands_item.php
@@ -1,5 +1,11 @@
 <?php
-if(isset($name)){
+$name_test = $name;
+$name = preg_replace("/[^a-zA-Z0-9_-ąćęłńóśźżĄĆĘŁŃÓŚŹŻ \.!]/", '', $name);
+if($name != $name_test){
+      die ('<h4 class="row entry-empty col-xs-12"><i class="fa fa-meh-o" aria-hidden="true"> Hola hola!!!! czy Ty usiłujesz zaatakować moją stronę?? <br> Twoje IP zostało zapisane do bazy danych</h4>');
+}
+else if(isset($name) && $name === $name_test){
+
    $stmt = $settings->selectBand($name);
    $num_rows = mysqli_num_rows($stmt);
    if($num_rows > 0){
@@ -18,4 +24,7 @@ if(isset($name)){
    else{
       die ('<h4 class="row entry-empty col-xs-12"><i class="fa fa-meh-o" aria-hidden="true"> brak w bazie zespołu o podanej nazwie</h4>');
    }
+}
+else{
+      die ('<h4 class="row entry-empty col-xs-12"><i class="fa fa-meh-o" aria-hidden="true"> Hmmm... coś poszło nie tak - spróbuj jeszcze raz </h4>');
 }
